a280cef0ac22e7385eab109861509b8e06cef71d
inakicarcereny <inakicarce@hotmail.com.ar>
2025-04-05 02:09:09 -0300

    .

---
+++ database/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(first_name, age, email)
+VALUES('pedro', 34, 'pedro@gmail.com');
---
+++ database/update.sql
@@ -0,0 +1,3 @@
+  UPDATE users SET age = 35 WHERE first_name = 'pedro';
+
+  UPDATE users SET age = 35, email = 'pedro@gmail.com.ar' WHERE first_name = 'pedro';
